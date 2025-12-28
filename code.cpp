cfdfe3a6c2017aa1fd6f09d09e1bec340e2fea58
evy-phoba <evaristephoba22@gmail.com>
2025-12-28 18:39:09 +0200

    Update code.cpp
    first part

--- code.cpp
+++ code.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 
+using namespace std;
+
+/*DATA STRUCTURES */
+
+struct Process {
+    int burstTime;
+    int priority;
+    int arrivalTime;
+    int waitingTime;
+    bool finished;
+    Process* next;
+};
